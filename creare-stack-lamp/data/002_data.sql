48c3298ae2f8d78f7919cb25f3be8b985aa13e68
Alessandro Lanni <alessandro.lanni@gmail.com>
2020-12-08 19:42:30 +0100

    Update 002_data.sql

---
+++ creare-stack-lamp/data/002_data.sql
@@ -0,0 +1,15 @@
+USE myschema;
+
+INSERT INTO `fruit` (`fruit_id`, `name`, `variety`) VALUES
+(1, 'Apple', 'Red Delicious'),
+(2, 'Pear', 'Comice'),
+(3, 'Orange', 'Navel'),
+(4, 'Pear', 'Bartlett'),
+(5, 'Orange', 'Blood'),
+(6, 'Apple', 'Cox''s Orange Pippin'),
+(7, 'Apple', 'Granny Smith'),
+(8, 'Pear', 'Anjou'),
+(9, 'Orange', 'Valencia'),
+(10, 'Banana', 'Plantain'),
+(11, 'Banana', 'Burro'),
+(12, 'Banana', 'Cavendish');
