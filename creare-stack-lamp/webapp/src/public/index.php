a02ebcc606f492390d4917f24bb000456a9771d1
Alessandro <alessandro.lanni@gmail.com>
2020-12-08 20:08:43 +0100

    fix: varie su index.php

--- creare-stack-lamp/webapp/src/public/index.php
+++ creare-stack-lamp/webapp/src/public/index.php
@@ -1,4 +1,40 @@
 <?php
 
-echo "Questo e' un Test!";
+/**
+ * Questo esempio serve a comprovare la connessione tra container
+ *
+ *
+ *
+**/
+
+$hostname = "datastore";
+
+$dbname = "myschema";
+
+$dbuser = "mydbuser";
+
+$dbpassword = "1234test";
+
+try{
+	// Tentativo di connessione al container Mariadb/DB
+	$connection = new PDO("mysql:host=$hostname;dbname=$dbname", $dbuser, $dbpassword);
+} catch(\PDOException $ex) {
+	echo "Errore di connesstione al DB: " . $ex->getMessage();
+	die();
+}
+
+$query = "SELECT * FROM fruit ORDER BY name";
+$stmt = $connection->prepare($query);
+$stmt->execute();
+
+echo "Questo e' l'elenco di frutti disponibili nel DB: <br>";
+
+
+echo "<ul>";
+
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+	echo "<li>" . $row['name']  . " - " .  $row['variety'] . "</li>";
+}
+
+echo "</ul>";
 
